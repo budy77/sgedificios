2616cb80990bb60e67c8b1c8fcd9077a2c6967ef
budy77 <ruddy.marquina@gmail.com>
2017-07-31 19:34:17 -0400

    primer commit agregados componentes de uso
    agregados nuevos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+import {AccordionModule} from 'primeng/primeng';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AccordionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
